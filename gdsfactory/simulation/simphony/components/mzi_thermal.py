3ac3f078323d4a1dfb538488913eb472c86a353b
Elam Day-Friedland <elamdf@eecs.berkeley.edu>
2023-03-07 10:43:37 -0800

    mzi_thermal mypy clean + added comments

--- gdsfactory/simulation/simphony/components/mzi_thermal.py
+++ gdsfactory/simulation/simphony/components/mzi_thermal.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 import numpy as np
 
 
-def delta_temperature(wavelength, length, dn=1.87e-4):
+def delta_temperature(
+    wavelength: np.float64, length: np.float64, dn: np.float64 = 1.87e-4
+) -> np.float64:
     """Return the delta temperature for a pi phase shift on a MZI interferometer."""
     return wavelength / 2 / length / dn
 
@@ -13,7 +15,8 @@ def test_delta_temperature() -> None:
     np.isclose(dt, 41.44385026737968)
     dt = delta_temperature(1.55, 1000)
     np.isclose(dt, 4.44385026737968)
+    # TODO maybe assert all(np.isclose) or something? This check doesn't do anything, isclose returns a bool array
 
 
 if __name__ == "__main__":
-    test_delta_temperature()
+    test_delta_temperature()  # FIXME this doesn't do anything- np.isclose isn't an assert
